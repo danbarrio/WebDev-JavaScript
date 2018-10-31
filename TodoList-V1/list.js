c42c2a5871b8084a3fec947a4c8da7dafa5c8be2
Dan Barrio <danbarrio@users.noreply.github.com>
2018-10-31 15:29:28 -0700

    Todo List V1

---
+++ TodoList-V1/list.js
@@ -0,0 +1,19 @@
+var todos = ["Buy new Turtle"];
+
+var input = prompt("What would you like to do?");
+
+while (input !== "quit") {
+	// handle input
+	if (input === "list") {
+		console.log(todos);
+		// ask for new todo
+	} else if (input === "new" ) {
+		var newTodo = prompt("Enter new todo");
+		// add to todos array
+		todos.push(newTodo);
+	}
+
+	// ask again for new input
+	var input = prompt("What would you like to do?");
+}
+console.log("OK, YOU QUIT THE APP");
