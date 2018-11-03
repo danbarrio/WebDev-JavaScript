fa9835a421b196de9582fef212b012f82009184c
Dan Barrio <danbarrio@users.noreply.github.com>
2018-11-03 11:29:59 -0700

    Array Iteration Examples
    Iterate through an Array using a For Loop and a forEach Loop

---
+++ Arrays/ArraysForEachLoop.js
@@ -0,0 +1,6 @@
+// Create an array and populate it
+var colors = ["red", "orange", "yellow", "green"];
+
+colors.forEach(function(item){
+	console.log(item);
+});
---
+++ Arrays/ArraysForLoop.js
@@ -0,0 +1,7 @@
+// Create an array and populate
+var colors = ["red", "orange", "yellow", "green"]
+
+// Iterate through the array and print each item to the console
+for(var i = 0; i < colors.length; i++){
+	console.log(colors[i]);
+}
