e1d1474984a7cd46af9c631a6f6f39a99ee07073
Dan Barrio <danbarrio@users.noreply.github.com>
2018-11-06 11:39:23 -0800

    Array Problem Set 1, optimize and continue

--- Arrays/ProblemSet1.js
+++ Arrays/ProblemSet1.js
@@ -1,27 +1,33 @@
 // 1. Print out the Array elements in reverse order //////////////////////
 
-// My Solution:
-var array1 = [1,2,3,4];
-
-printReverse(array1);
-
-function printReverse(item)
+function printReverse(arr)
+{
+	for(var i = arr.length-1; i >= 0; i--)
 	{
-		//console.log(item.length);
-
-		for(var i = item.length; i > -1; i--)
-		{
-			console.log(item[i]); 
-		}
-
+		console.log(arr[i]);
 	}
+}
 
-// Course Solution:
+printReverse([1,2,3,4]);
 
 
 // 2. Check if all items in the array are the same. Print True or False //////////////////////
 
-var array2 = [1,1,1,1]
-var array3 = [2,1,1,1]
-var array4 = ["a","b","p"]
-var array5 = ["b","b","b"]
+function isUniform(arr)
+{
+	// The first item in the Array	
+	var first = arr[0];
+
+	// run through the Array, starting at index 1
+	for(var i = 1; i < arr.length; i++)
+	{
+		// Compare every index starting at 1 to var first;
+		if(arr[i] !== first)
+		{
+			return false;
+		}
+	}
+	return true;
+}
+
+// run in console:   isUniform([1,1,1,1])
