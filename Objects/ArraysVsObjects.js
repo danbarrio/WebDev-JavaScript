6324923197304cc9bfb91afda1c3c99bae7f4f70
Dan Barrio <danbarrio@users.noreply.github.com>
2018-11-07 17:12:59 -0800

    Arrays vs Objects

---
+++ Objects/ArraysVsObjects.js
@@ -0,0 +1,23 @@
+// ARRAYS ////////////////////////
+
+var dogs = ["Rusty", "Lucky", "Rex", "Bubba"];
+
+// To get an item from an array, use its index number:  dogs[1]
+
+// To add an item to the end of an array use push:  dogs.push("Rover")
+
+
+// OBJECTS ////////////////////////
+
+var dog = 
+{
+	name: "Rusty",
+	breed: "Lab",
+	age: 6
+}
+
+// To get info from and object, use either square brackets with property:  dog["name"]
+// or dot notation:  dog.name
+
+// To add a property in an object, use brackets:  dog["color"] = "Black" 
+// or dot notation:  dog.color = black
