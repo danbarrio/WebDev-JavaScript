9a1abd0d29ebb60674e1b34889532eb37b6b67f2
Dan Barrio <danbarrio@users.noreply.github.com>
2018-11-13 12:12:27 -0800

    Example: "this" keyword in a Method

--- Methods/methods.js
+++ Methods/methods.js
@@ -11,3 +11,23 @@ var obj = {
 }
 
 // to call function: obj.add(4,2);
+
+
+// You can add a new property and values for it using dot notation:
+
+obj.favColors = ["Blue", "Green"];
+
+
+// Using "this" keyword in a Method refers to the parent object it is in, 
+// in this case serves as a replacement for comments.data.forEach...
+
+var comments = {
+	data: ["Great Job!", "Bye", "Uh-Huh"],
+	print: function(){
+		this.data.forEach(function(element){
+		console.log(element);	
+		});
+	}
+};
+
+// to call the print function: comments.print()
