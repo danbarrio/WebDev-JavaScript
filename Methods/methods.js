88974990f5116104fd3b2137496058f760b1212e
Dan Barrio <danbarrio@users.noreply.github.com>
2018-11-13 11:57:02 -0800

    Example: Methods in JavaScript

--- Methods/methods.js
+++ Methods/methods.js
@@ -1,1 +1,13 @@
-prompt("Connected");
+// In JavaScript, a Method is a Property which is a Function inside an object.
+// Like all properties of an object, it can be called with dot notation (pass in any parameters).
+
+var obj = {
+	name: "Chuck",
+	age: 51,
+	friends: ["Joe", "Fred", "Mary"],	
+	add: function(x,y){
+		return x + y;
+	}
+}
+
+// to call function: obj.add(4,2);
