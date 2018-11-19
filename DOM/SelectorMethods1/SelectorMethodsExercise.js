82541caadda93999660d2a8321a3cd90a5c4824c
Dan Barrio <danbarrio@users.noreply.github.com>
2018-11-18 18:27:41 -0800

    Selector Methods Exercise

---
+++ DOM/SelectorMethods1/SelectorMethodsExercise.js
@@ -0,0 +1,7 @@
+var tag1 = document.getElementById("first");
+
+var tag2 = document.querySelector("#first");
+
+var tag3 = document.getElementsByClassName("special")[0];
+
+var tag4 = document.getElementsByTagName("p")[0];
