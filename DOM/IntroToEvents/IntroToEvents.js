53764786666fde94e0aeac77e6751be93fd3bf13
Dan Barrio <danbarrio@users.noreply.github.com>
2018-11-20 17:00:47 -0800

    Intro To Events 1

---
+++ DOM/IntroToEvents/IntroToEvents.js
@@ -0,0 +1,8 @@
+var button = document.querySelector("button");
+var paragraph = document.querySelector("p");
+
+// SETUP CLICK LISTENER
+button.addEventListener("click", function() {
+	paragraph.textContent = "Someone Clicked the Button!"
+});
+
