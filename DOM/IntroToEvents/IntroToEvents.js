c32673f8d08775f959a67493cdc65a6a294d6584
Dan Barrio <danbarrio@users.noreply.github.com>
2018-11-20 17:23:07 -0800

    Intro To Events 2

--- DOM/IntroToEvents/IntroToEvents.js
+++ DOM/IntroToEvents/IntroToEvents.js
@@ -6,3 +6,12 @@ button.addEventListener("click", function() {
 	paragraph.textContent = "Someone Clicked the Button!"
 });
 
+// Find all Li's in page and put them into an array: lis
+var lis = document.querySelectorAll("li");
+
+// Add an on Click EventListener to each li in array
+for(var i = 0; i < lis.length; i++){
+	lis[i].addEventListener("click", function(){
+		this.style.color = "red";
+	});
+}
