210434ac87018d0077e9c864be65d870ca11e493
Dan Barrio <danbarrio@users.noreply.github.com>
2018-11-20 17:28:02 -0800

    Intro to Events 3

--- DOM/IntroToEvents/IntroToEvents.js
+++ DOM/IntroToEvents/IntroToEvents.js
@@ -1,10 +1,19 @@
 var button = document.querySelector("button");
 var paragraph = document.querySelector("p");
 
-// SETUP CLICK LISTENER
+// SETUP CLICK LISTENER with un-named Function
+/*
 button.addEventListener("click", function() {
 	paragraph.textContent = "Someone Clicked the Button!"
 });
+*/
+
+// SETUP CLICK LISTENER with named Function
+button.addEventListener("click", changeText);
+
+function changeText(){
+	paragraph.textContent = "Someone Clicked the Button!"
+}
 
 // Find all Li's in page and put them into an array: lis
 var lis = document.querySelectorAll("li");
