5aedf3c7d4b68e2f6c5fd49fdbc92fd54305729a
Dan Barrio <danbarrio@users.noreply.github.com>
2018-11-24 13:26:09 -0800

    Update player 1 score on button press

--- DOM/ScoreKeeper/score.js
+++ DOM/ScoreKeeper/score.js
@@ -1,1 +1,11 @@
-alert("Connected");
+var p1button = document.querySelector("#p1");
+// variation: getElementById
+var p2button = document.getElementById("p2");
+var p1Display = document.querySelector("#p1Display");
+var h1 = document.querySelector("h1");
+var p1Score = 0;
+
+p1button.addEventListener("click", function(){
+	p1Score++;
+	p1Display.textContent = p1Score;
+});
